cbc457270e1f736a6e1546782da69c57b1fff0dd
Austenyt <austenyt@gmail.com>
2024-01-18 09:42:00 +0300

    some

--- goods/views.py
+++ goods/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
 
 from goods.models import Category, Product
 
 
-class CategoriesListView(ListView):
-    model = Product
-    template_name = 'goods/index.html'
-
-
-# def index(request):
-#     context = {
-#         'object_list': Category.objects.all()[:3],
-#         'title': 'Магазин для фанатов кантри'
-#     }
-#     return render(request, 'goods/index.html', context)
+def index(request):
+    context = {
+        'object_list': Category.objects.all()[:3],
+        'title': 'Магазин для фанатов кантри'
+    }
+    return render(request, 'goods/index.html', context)
 
 
 def contacts(request):
